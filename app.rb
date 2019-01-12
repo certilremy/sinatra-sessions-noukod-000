ff29d0246f576c198c171ea644cb80543c1064cf
Certil Remy <certil@noukod.com>
2019-01-12 02:37:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,11 +32,13 @@ class App < Sinatra::Base
   end
 
   get '/second_exercise' do
-    "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
+    "Your second lesson will be to set your session :id key to a value.\nIn the 
+    route:/set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
   end
 
   get '/set_session' do
     #set session id here
+    session[:id] = 1
 
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
